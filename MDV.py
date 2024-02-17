9ab83849e3f8e222b68a0b46442f058bef4aedd4
ERICK-DANIEL <146472894+ERICK-DANIEL@users.noreply.github.com>
2024-02-16 18:52:59 -0600

    Add files via upload

--- MDV.py
+++ MDV.py
@@ -1,18 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Feb 16 12:26:35 2024
+Created on Fri Feb 16 18:46:36 2024
 
-@author: zS23021810
+@author: erick
 """
 
-Numero1 = int(input("Ingresa el primer numero: "))
-resto = int(input("Ingresa el segundo numero: "))
+num1 = int(input("Ingresa el primer numero: "))
+num2 = int(input("Ingresa el segundo numero: "))
 
-while (Numero1 != 0 and resto != 0):
-    resto = Numero1 % resto
-    Numero1 = resto
+while num2 > 0:
+    num1, num2 = num2, num1 % num2
     
-    print(resto)
-
-    
-    
+print(num1)
